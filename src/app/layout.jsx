493774216be3503b0b6572cc4f7bf493b3b91cc6
Yenser1 <yenserdavidvasquezhernandez@gmail.com>
2025-07-01 22:48:51 -0400

    interactivity to the header

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -17,6 +17,39 @@ export const metadata = {
   description: "Punta Cana Fun Website",
 };
 
+
+function HamburguerMenu(){
+  return (
+    <div className="absolute top-1/2 right-3 cursor-pointer"
+    >
+          <svg
+            width="52"
+            height="52"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M2 6C2 5.44772 2.44772 5 3 5H21C21.5523 5 22 5.44772 22 6C22 6.55228 21.5523 7 21 7H3C2.44772 7 2 6.55228 2 6Z"
+              fill="currentColor"
+            />
+            <path
+              d="M2 12.0322C2 11.4799 2.44772 11.0322 3 11.0322H21C21.5523 11.0322 22 11.4799 22 12.0322C22 12.5845 21.5523 13.0322 21 13.0322H3C2.44772 13.0322 2 12.5845 2 12.0322Z"
+              fill="currentColor"
+            />
+            <path
+              d="M3 17.0645C2.44772 17.0645 2 17.5122 2 18.0645C2 18.6167 2.44772 19.0645 3 19.0645H21C21.5523 19.0645 22 18.6167 22 18.0645C22 17.5122 21.5523 17.0645 21 17.0645H3Z"
+              fill="currentColor"
+          />
+    </svg>
+        <div className=""
+        >
+        </div>
+    </div>
+  )
+}
+
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
@@ -31,7 +64,8 @@ export default function RootLayout({ children }) {
                 color: 'white'
               }}
       >
-        <Image src="/PuntaCana.png"
+        <a href="/">
+          <Image src="/PuntaCana.png"
               width={2000}
               height={1000}
               alt="Punta Cana Fun"
@@ -39,20 +73,25 @@ export default function RootLayout({ children }) {
                 filter: 'drop-shadow(0 0 25px #333)',
                 width: 'clamp(200px, 20vw, 600px)',
               }}
+              className="hover:scale-110 transition-transform duration-350"
         />
+        </a>
+            
+           {HamburguerMenu()} 
+              
         <div>
-          <ul className="flex gap-5 navlinks font-mono"
+          <ul className="flex gap-10 navlinks font-mono"
           >
-            <a href="/">
+            <a href="/" className="hover:underline hover:text-amber-300 transition-colors duration-250">
             <li>Inicio</li>
             </a>
-            <a href="./Acerca-De-Nosotros">
+            <a href="./Acerca-De-Nosotros" className="hover:underline hover:text-amber-300 transition-colors duration-250">
             <li>Acerca de Nosotros</li>
             </a>
-            <a href="./Contacto">
+            <a href="./Contacto" className="hover:underline hover:text-amber-300 transition-colors duration-250">
             <li>Contacto</li>
             </a>
-            <a href="./Tours-Y-Excursiones">
+            <a href="./Tours-Y-Excursiones" className="hover:underline hover:text-amber-300 transition-colors duration-250">
             <li>Tours y Excursiones</li>
             </a>
           </ul>
